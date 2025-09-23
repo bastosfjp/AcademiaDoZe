a607d60417d037322398a17bf4a648ed6b51e946
bastosfjp <jpbastosfernandes@gmail.com>
2025-09-22 21:15:06 -0300

    Primeira Versão do Maui

---
+++ AcademiaDoZe.Presentation.AppMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AcademiaDoZe.Presentation.AppMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
